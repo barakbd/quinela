791b10b34df5fe8687694aa8d107016e00b9e37e
Barak Ben-David <barakbd@live.com>
2016-08-30 19:20:24 -0700

    ng-cookies, direct registration

--- client/static/js/controllers.js
+++ client/static/js/controllers.js
@@ -1,31 +1,45 @@
-futgame_app.controller('loginController', function ($scope, $cookies, $location, loginFactory) {
+futgame_app.controller('loginController', function($scope, $cookies, $location, loginFactory) {
 
-  $scope.login = function () {
-    loginFactory.login($scope.loginUser, function (data){
-        $location.url('/dashboard');
-    });
-}
+    $scope.login = function() {
+        console.log('loginController - login');
+        loginFactory.login($scope.loginUser, function(response) {
+            console.log('loginController - login', response);
+            $cookies.put('id', response.id);
+            $cookies.put('username', response.username);
+            console.log('cookies all', $cookies.getAll());
+            $scope.currentUser = {
+                id: $cookies.get('id'),
+                username: $cookies.get('username'),
+            };
+            console.log('currentUser', $scope.currentUser);
+            $location.url('/dashboard');
+        });
+    };
 
-  $scope.register = function () {
-    loginFactory.register($scope.newUser, function (data){
-      if (data.data.errors){
-        $scope.errors = data.data.errors;
-      }
-      else{
-        $scope.user = data.data;
-        $location.path('/dashboard');
-      }
-    }, function(err){
-      console.log("I am an error",err);
-    });
-  };
+    $scope.register = function() {
+        loginFactory.register($scope.newUser, function(response) {
+            console.log('loginController - register - ', response);
+            if (!response.success) {
+                $scope.errors = response.msg;
+            } else {
+                $scope.loginUser.email = $scope.newUser.email;
+                $scope.loginUser.password = $scope.newUser.password;
+                loginFactory.login($scope.loginUser, function(response) {
+                    $location.url('/dashboard');
+                });
+            }
+        });
+    };
 
-  $scope.logout = function () {
-    console.log('hi cont');
-      loginFactory.logout(function (data) {
-        $location.path('/login');
-      })
-  }
+    $scope.logout = function() {
+        console.log('hi cont');
+        loginFactory.logout(function(data) {
+            $cookies.remove('id');
+            $cookies.remove('username');
+            console.log('cookies cleared on logout - ', $cookies.getAll());
+            $location.path('/login');
+        });
+    };
 
 
 });
@@ -33,25 +47,41 @@ futgame_app.controller('loginController', function ($scope, $cookies, $location,
 
 
 //deleteme after we create dashboard
-futgame_app.controller('dashboardController', function ($scope, loginFactory, dashboardFactory) {
+futgame_app.controller('dashboardController', function($scope, $cookies, $location, loginFactory, dashboardFactory) {
+    $scope.currentUser = {
+        id: $cookies.get('id'),
+        username: $cookies.get('username'),
+    };
+    console.log('currentUser', $scope.currentUser);
+    $location.url('/dashboard');
 
 
-  dashboardFactory.index(function(data){
-    $scope.personList = data;
-  });
-
-  $scope.addPerson = function () {
-    namesFactory.create($scope.newPerson, function (data) {
-      $scope.personList = data;
-      $scope.newPerson = {};
+    dashboardFactory.index(function(data) {
+        $scope.personList = data;
     });
-  };
 
-  $scope.removePerson =  function (person) {
-    namesFactory.delete(person, function (data) {
-      $scope.personList = data;
-    });
-  };
+    $scope.addPerson = function() {
+        namesFactory.create($scope.newPerson, function(data) {
+            $scope.personList = data;
+            $scope.newPerson = {};
+        });
+    };
+
+    $scope.removePerson = function(person) {
+        namesFactory.delete(person, function(data) {
+            $scope.personList = data;
+        });
+    };
+
+    $scope.logout = function() {
+        console.log('hi cont');
+        loginFactory.logout(function(data) {
+            $cookies.remove('id');
+            $cookies.remove('username');
+            console.log('cookies cleared on logout - ', $cookies.getAll());
+            $location.path('/login');
+        });
+    };
 
 
 
--- client/static/js/factories.js
+++ client/static/js/factories.js
@@ -1,57 +1,59 @@
-futgame_app.factory('loginFactory', function ($http) {
-  var factory = {};
+futgame_app.factory('loginFactory', function($http) {
+    var factory = {};
 
 
-  factory.login = function (user, callback) {
-    $http.post('/login', user).success(function (output) {
-      callback(output);
-    })
-  };
+    factory.login = function(user, callback) {
+        console.log('loginFactory - login', user);
+        $http.post('/login', user).success(function(response) {
+            console.log('loginFactory - login', response);
+            callback(response);
+        });
+    };
 
-  factory.register = function (newUser, callback) {
-      console.log('register' + newUser);
-      $http.post('/signup', newUser).success(function (output) {
-        callback(output);
-      });
-  };
+    factory.register = function(newUser, callback) {
+        console.log('loginFactory - register', newUser);
+        $http.post('/signup', newUser).success(function(response) {
+            callback(response);
+        });
+    };
 
-  factory.logout = function (callback) {
-    console.log('hi fact');
-    $http.get('/logout').success(function (output) {
-      console.log('hi success');
-      callback(output);
-    });
-  };
+    factory.logout = function(callback) {
+        console.log('hi fact');
+        $http.get('/logout').success(function(output) {
+            console.log('hi success');
+            callback(output);
+        });
+    };
 
 
-  return factory;
+    return factory;
 });
 
 
-futgame_app.factory('dashboardFactory', function ($http) {
-  var factory = {};
-  var personList = [];
+futgame_app.factory('dashboardFactory', function($http) {
+    var factory = {};
+    var personList = [];
 
-  //Restful syntax: index = get all that object
-  factory.index = function(callback) {
+    //Restful syntax: index = get all that object
+    factory.index = function(callback) {
 
-      $http.get('/pools').success(function(output) {
-      callback(output);
-    })
-  };
+        $http.get('/pools').success(function(output) {
+            callback(output);
+        })
+    };
 
-  factory.create = function (newPerson, callback) {
-    $http.get('/new/' + newPerson.name + '/').success(function (output) {
-      callback(output);
-    })
-  };
+    factory.create = function(newPerson, callback) {
+        $http.get('/new/' + newPerson.name + '/').success(function(output) {
+            callback(output);
+        })
+    };
 
-  factory.delete = function (person, callback) {
-      $http.get('/remove/'+ person.name +'/').success(function (output) {
-        callback(output);
-      })
-  };
+    factory.delete = function(person, callback) {
+        $http.get('/remove/' + person.name + '/').success(function(output) {
+            callback(output);
+        })
+    };
 
 
-  return factory;
+    return factory;
 });
